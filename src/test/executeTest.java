79cd523f7cc872064377c3314dc369a7d51cb2ad
ukumar <ukumar@myvest.com>
2019-11-29 15:23:10 +0530

    testing stash

--- src/test/executeTest.java
+++ src/test/executeTest.java
@@ -18,6 +18,7 @@ public class executeTest extends getDriver {
 	   LoginPage loginpage=new LoginPage(driver);
 	  loginpage.LoginApp("TLT511", "xW7Sxp");
 	  System.out.println("Testing a code");
+	  System.out.println("Trying to stash to the local machine");
 	}
 	
 }
