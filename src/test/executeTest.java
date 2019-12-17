ac41f472b3a59d1a558a29487f8fe4e2e761a486
ukumar <ukumar@myvest.com>
2019-12-17 05:52:12 +0530

    resolved all commit

--- src/test/executeTest.java
+++ src/test/executeTest.java
@@ -17,7 +17,11 @@ public class executeTest extends getDriver {
 	public  void LoginIntoApp(){
 	   LoginPage loginpage=new LoginPage(driver);
 	  loginpage.LoginApp("TLT511", "xW7Sxp");
+
 	  System.out.println("Testing a code");
+
+	  System.out.println("Testing a code on dev");
+
 	  System.out.println("Trying to stash to the local machine");
 
 	  System.out.println("Right now switching to master, let me work on master first then I will switch back dev and get dev chages");
