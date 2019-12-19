183d1b003d1614b6c56cd253623d65a23379d43b
ukumar <ukumar@myvest.com>
2019-12-19 17:01:45 +0530

    updated the constant variable

--- src/com/sakValut/utility/ConstantVariables.java
+++ src/com/sakValut/utility/ConstantVariables.java
@@ -3,7 +3,7 @@ package com.sakValut.utility;
 public class ConstantVariables {
 	
 	public static String appProp="//src//resources//application.properties";
-	public static String appURL="https://saksoft.empxtrack.com/empxtrack/logon.do?activity=display&location=Saksoft";
+	public static String appURL="https://sakvault.saksoft.com/user/login";
 	public static String chromeDriver="Drivers\\chromedriver.exe";
 
 }
